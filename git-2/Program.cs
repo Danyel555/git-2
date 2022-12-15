ae9d3d92bdefa8917b8835fed5b4c451c3cdfc8f
Danyel555 <113988414+Danyel555@users.noreply.github.com>
2022-12-15 10:27:35 +0300

    Update Program.cs

--- git-2/Program.cs
+++ git-2/Program.cs
@@ -1,40 +1,65 @@
-Random RandomClass = new Random();
-int x = RandomClass.Next(1, 100);
-Console.WriteLine("Число от 1 до 100, угадай какое?");
-int guess = 0;
-while (guess != x)
+int fag;
+while (true)
 {
-    guess = Convert.ToInt32(Console.ReadLine()); 
-    if (guess == x)
+    Console.WriteLine("Меню\n" +
+        "1-Угадай число\n" +
+        "2-Таблица умножения\n" +
+        "3-Вывод делителей числа\n" +
+        "4-Выход из программы\n");
+    Console.WriteLine("Выберите пункт меню: ");
+    fag = Convert.ToInt32(Console.ReadLine());
+    switch (fag)
     {
-        Console.WriteLine("Вы угадали!");
-        Console.ReadLine(); 
+        case 1:
+            Random r = new Random();
+            int rn = r.Next(0, 100);
+            while (true)
+            {
+                Console.WriteLine("Введите число: ");
+                string t = Console.ReadLine();
+                int mn = Convert.ToInt32(t);
+                if (rn > mn)
+                {
+                    Console.WriteLine("Больше");
+                }
+                if (rn < mn)
+                {
+                    Console.WriteLine("Меньше");
+                }
+                if (rn == mn)
+                {
+                    Console.WriteLine("Вы выиграли!");
+                    break;
+                }
+            }
+            break;
+        case 2:
+            var table = new int[10, 10];
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    table[i, j] = i * j;
+                }
+            }
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    Console.Write("{0, 3}", table[i, j]);
+                }
+                Console.WriteLine();
+            }
+            break;
+        case 3:
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+                if (num % i == 0)
+                    Console.WriteLine("{0}/{1}", num, i);
+            break;
+        case 4:
+            Environment.Exit(0);
+            break;
     }
-    else if (guess != x)
-    {
-        if (guess < x)
-        {
-            Console.WriteLine("Число меньше");
-            Console.ReadLine(); 
-        }
-        else if (guess > x)
-        {
-            Console.WriteLine("Число больше");
-            Console.ReadLine(); 
-        }
-    }
-    Console.ReadLine(); 
-}
-
-test();
-
-void table()
-{
-throw new NotImplementedException();
-}
-static void test(int[] numbers)
-{
-    var Table = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    for (var i = 0; i < numbers.Length; i++)
-        numbers[i] = numbers[i] * numbers[i];
 }
